8c76ebee5faac4022775fb79c325273da504e806
root <root@ubuntu.lyij53dfx50unltfuqyx4puika.lx.internal.cloudapp.net>
2019-11-03 17:57:17 +0900

    add todolist-vue

---
+++ sample4/vue/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <todo-list></todo-list>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import TodoList from '@/components/TodoList.vue';
+
+export default Vue.extend({
+  name: 'home',
+  components: {
+    TodoList,
+  },
+});
+</script>
